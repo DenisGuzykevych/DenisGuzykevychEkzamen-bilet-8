ca2b61056d01478ee7d5a026073e09b3d7b6f003
DenisGuzykevych <118122975+DenisGuzykevych@users.noreply.github.com>
2022-12-15 20:26:43 +0200

    Add files via upload

--- Гузикевич Денис гр 1-2 білет №8.cpp
+++ Гузикевич Денис гр 1-2 білет №8.cpp
@@ -20,4 +20,31 @@ int main()
     for (int i = 0; i < 8; i++)
         sum += a[i];
     cout << "Сума eлементiв масиву = " << sum << endl;
+
+    int k = 8 - 1;
+    int p = 8 - 2;
+    if (8 % 2 == 0)
+    {
+        k = 8 - 2;
+        p = 8 - 1;
+    }
+    for (int i = 0; i < 8 - 2; i += 2)
+        for (int j = k; j > i; j -= 2)
+            if (a[j] < a[j - 2])
+            {
+                int t = a[j];
+                a[j] = a[j - 2];
+                a[j - 2] = t;
+            }
+    for (int i = 1; i < 8 - 2; i += 2)
+        for (int j = p; j > i; j -= 2)
+            if (a[j] < a[j - 2])
+            {
+                int t = a[j];
+                a[j] = a[j - 2];
+                a[j - 2] = t;
+            }
+    cout << "Масив у порядку зростання по парним та непарним числам: \n" << endl;
+    for (int i = 0; i < 8; i++)
+        cout << a[i] << endl;
 }
