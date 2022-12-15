6d5f5e2fddf4e8ad30e17a7c751a6d01b09bcfb4
DenisGuzykevych <118122975+DenisGuzykevych@users.noreply.github.com>
2022-12-15 15:19:36 +0200

    Add files via upload

---
+++ Гузикевич Денис гр 1-2 білет №8.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream> 
+using namespace std;
+
+int main()
+{
+    setlocale(0, ".1251");
+    int a[8];
+
+    int dobutok = 1, sum = 0;
+    
+    cout << "Введiть елементи масиву \n";
+    for (int i = 0; i < 8; i++)
+    {
+        cin >> a[i];
+        if(a[i] > 0)
+            dobutok *= a[i];
+    }
+    cout << "Добуток додатних чисел масиву = " << dobutok << endl;
+   
+    for (int i = 0; i < 8; i++)
+        sum += a[i];
+    cout << "Сума eлементiв масива = " << sum << endl;
+}
